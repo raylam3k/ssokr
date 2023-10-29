a51bfaaae6fbd97e1f4d612da9555c92680842ec
raylam3k <40323507+raylam3k@users.noreply.github.com>
2023-10-29 19:52:13 +0000

    Commit Asset.py

--- assets.py
+++ assets.py
@@ -1,4 +1,6 @@
 print( "this is a new file I added")
 
 print( "to make it to git I need to go to SOURCE CONTROL")
-print( "the file should have a U next to it")
+print( "the file should have a U (Untrack) next to it")
+print( "Hit the + and it becomes A (Add) then M (Modified)")
+
