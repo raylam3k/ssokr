60ec7362b9e4b34efdcdcee51bb923f823ee743b
raylam3k <40323507+raylam3k@users.noreply.github.com>
2023-10-30 15:26:32 +0000

    Add docstring on func

--- assets.py
+++ assets.py
@@ -8,6 +8,7 @@ print( "the file should have a U (Untrack) next to it")
 print( "Hit the + and it becomes A (Add) then M (Modified)")
 
 def my_func( x, y):
+    """simple add func"""
     return x+y
 
 print( my_func(3,2 ))
