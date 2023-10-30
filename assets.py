deee76e0c723108e601b406406018f8a2990b20f
raylam3k <40323507+raylam3k@users.noreply.github.com>
2023-10-30 13:37:23 +0000

    add back pandas

--- assets.py
+++ assets.py
@@ -7,10 +7,10 @@ print( "to make it to git I need to go to SOURCE CONTROL")
 print( "the file should have a U (Untrack) next to it")
 print( "Hit the + and it becomes A (Add) then M (Modified)")
 
-
 def my_func( x, y):
     return x+y
 
 print( my_func(3,2 ))
 print( my_func(4,5 ))
+
 a = pd.DataFrame()
