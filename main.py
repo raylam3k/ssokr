7daba0777cc7f9826a2bae28a74a3bf95ff3cb8e
raylam3k <40323507+raylam3k@users.noreply.github.com>
2023-10-30 13:30:42 +0000

    formatting

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import numpy as np   # how do I konw which pacakge is preloaded
 import pandas as pd  # both works
 # import icecream as ic   # do pip install icecream in terminal
-  
+
 for i in range(10):
-    print( i )
+    print(i)
 
-for j in ( 'a', 'b', 'c'):
-    print( j)
+for j in ('a', 'b', 'c'):
+    print(j)
 
-a = np.arange(20).reshape(5,4)
+a = np.arange(20).reshape(5, 4)
 b = np.arange(20)
 
 c = pd.DataFrame()
-print( c )
-
+print(c)
