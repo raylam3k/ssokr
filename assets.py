1864c40f9948e6e1cd6b0074688aba2e6b91252d
raylam3k <40323507+raylam3k@users.noreply.github.com>
2023-10-30 08:57:01 -0400

    add some random code

--- assets.py
+++ assets.py
@@ -4,3 +4,8 @@ print( "to make it to git I need to go to SOURCE CONTROL")
 print( "the file should have a U (Untrack) next to it")
 print( "Hit the + and it becomes A (Add) then M (Modified)")
 
+
+def my_func( x, y):
+    return x+y
+
+print( my_func(3,2 ))
